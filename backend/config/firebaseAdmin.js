498a8533e03d6e2123973edee810fa4d404cbd52
R0NIN007 <akshayjohnson17@gmail.com>
2026-01-09 00:34:41 +0530

    working localhost

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,13 +1,21 @@
 import admin from "firebase-admin";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-  );
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// Load the service account key JSON file
+
+const serviceAccountPath =
+  "C:\\Users\\aksha\\Downloads\\serviceAccountKey.json";
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+
+// Initialize the Firebase admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 export default admin;
