82da3c9263def64ff775c1f394a4bb6a1ab854e3
R0NIN007 <akshayjohnson17@gmail.com>
2026-01-08 16:27:37 +0530

    deploy stage

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { sendPushNotification } from "../utils/sendpush.js";
 import { emailExistsAnywhere } from "../utils/checkEmailExists.js";
-import EmailOtp from "../models/EmailOtp.js";
+import EmailOtp from "../models/emailOtp.js";
 import { generateOtp, sendEmailOtp } from "../utils/emailOtp.js";
 
 // send email otp
