abd4cea7a1d18223054d4cde9fbb88c6e5fc94d6
R0NIN007 <akshayjohnson17@gmail.com>
2026-01-10 20:19:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,6 @@ app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 /* ================= ROUTES ================= */
 app.use("/api/customers", customerRoutes);
