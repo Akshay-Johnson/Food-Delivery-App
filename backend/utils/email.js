5ee82f2f7bb68171261924f75ea7bbf33ac761ef
R0NIN007 <akshayjohnson17@gmail.com>
2026-01-11 17:18:50 +0530

    change

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -6,7 +6,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
